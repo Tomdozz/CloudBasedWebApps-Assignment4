c7f3b633efec296bd75c815b3b697c6bebf428b7
tomdozz <tom_1995@msn.com>
2018-05-22 09:34:10 +0200

    Migrations done

---
+++ database/migrations/2018_05_22_070945_create_orderinprog_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderinprogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderinprogs', function (Blueprint $table) {
+          $table->increments('id');
+          $table->text('title');
+          $table->text('description');
+          $table->text('expecteddate');
+          $table->text('timesincestart');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderinprogs');
+    }
+}
