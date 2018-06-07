c581783b193389728b14885962d849b7f270d09f
tomdozz <tom_1995@msn.com>
2018-06-07 12:52:04 +0200

    Changed back blueprint file

--- resources/views/blueprint.blade.php
+++ resources/views/blueprint.blade.php
@@ -13,7 +13,7 @@
       top: 7px;
     }
     a{
-      color: black;
+      color: pink;
     }
     a:hover {
         color: white;
@@ -22,7 +22,7 @@
     </style>
     </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="/">Smide by P.O Andersson</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
